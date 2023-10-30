d11b9b893fcd78314b3384fd71d538dd4c98c19d
AbhishekHM <hmabhishek9700@gmail.com>
2023-10-30 15:07:40 +0530

    first commit

---
+++ notification_app/views.py
@@ -0,0 +1,8 @@
+
+# from django.shortcuts import render
+
+# # Create your views here.
+# def indexfile(request):
+#     return render(request,"myapp/index.html",{
+#         'room_name': "broadcast"
+#     })
